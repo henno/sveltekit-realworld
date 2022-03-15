4dd083521b2f5c55f8dbf5483643e66f67107d4f
Henno Täht <henno.taht@gmail.com>
2022-03-15 17:13:07 +0200

    Add Prisma

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,40 +1,66 @@
 const base = 'https://conduit.productionready.io/api';
+import {PrismaClient} from '@prisma/client'
 
-async function send({ method, path, data, token }) {
-	const opts = { method, headers: {} };
+async function send({method, path, data, token}) {
 
-	if (data) {
-		opts.headers['Content-Type'] = 'application/json';
-		opts.body = JSON.stringify(data);
-	}
+    const prisma = new PrismaClient()
 
-	if (token) {
-		opts.headers['Authorization'] = `Token ${token}`;
-	}
+    async function main() {
+        await prisma.Todo.create({
+            data: {
+                uid: 'fafafa',
+                created_at: new Date,
+                text: 'foo',
+                done: true,
+            },
+        })
 
-	return fetch(`${base}/${path}`, opts)
-		.then((r) => r.text())
-		.then((json) => {
-			try {
-				return JSON.parse(json);
-			} catch (err) {
-				return json;
-			}
-		});
+        const allUsers = await prisma.Todo.findMany({})
+        console.dir(allUsers, {depth: null})
+    }
+
+    main()
+        .catch((e) => {
+            throw e
+        })
+        .finally(async () => {
+            await prisma.$disconnect()
+        })
+
+    const opts = {method, headers: {}};
+
+    if (data) {
+        opts.headers['Content-Type'] = 'application/json';
+        opts.body = JSON.stringify(data);
+    }
+
+    if (token) {
+        opts.headers['Authorization'] = `Token ${token}`;
+    }
+
+    return fetch(`${base}/${path}`, opts)
+        .then((r) => r.text())
+        .then((json) => {
+            try {
+                return JSON.parse(json);
+            } catch (err) {
+                return json;
+            }
+        });
 }
 
 export function get(path, token) {
-	return send({ method: 'GET', path, token });
+    return send({method: 'GET', path, token});
 }
 
 export function del(path, token) {
-	return send({ method: 'DELETE', path, token });
+    return send({method: 'DELETE', path, token});
 }
 
 export function post(path, data, token) {
-	return send({ method: 'POST', path, data, token });
+    return send({method: 'POST', path, data, token});
 }
 
 export function put(path, data, token) {
-	return send({ method: 'PUT', path, data, token });
+    return send({method: 'PUT', path, data, token});
 }
